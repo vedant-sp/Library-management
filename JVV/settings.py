4c0c07ff24ffc101986ac5fb51be733c0365bd21
vedant-sp <55780747+vedant-sp@users.noreply.github.com>
2020-12-07 16:36:06 +0530

    Update settings.py

--- JVV/settings.py
+++ JVV/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'vaoutvpl11^d_*z443bfmyjqw#cq9!ld7y*6n#go-0s^m*!897'
+SECRET_KEY = ******************
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
